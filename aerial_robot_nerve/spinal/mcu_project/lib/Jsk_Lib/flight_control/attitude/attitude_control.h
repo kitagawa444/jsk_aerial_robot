efcc18b7db4d1014748fdf84ee69e3f89a55052d
kitagawa444 <kitagawa@dragon.t.u-tokyo.ac.jp>
2025-06-26 11:17:11 +0900

    tmp

--- aerial_robot_nerve/spinal/mcu_project/lib/Jsk_Lib/flight_control/attitude/attitude_control.h
+++ aerial_robot_nerve/spinal/mcu_project/lib/Jsk_Lib/flight_control/attitude/attitude_control.h
@@ -60,7 +60,7 @@
 
 /* fail safe */
 #define FLIGHT_COMMAND_TIMEOUT 500 //500ms
-#define MAX_TILT_ANGLE 2.0 // rad
+#define MAX_TILT_ANGLE 10.0 // rad
 #define MAX_PWM 1.0f // duty
 
 #define CONTROL_TERM_PUB_INTERVAL 100
