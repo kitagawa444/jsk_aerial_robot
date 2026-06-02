ebb1303be4d3257330f240b62fe2ee4c09cd7bea
kitagawa444 <kitagawa@dragon.t.u-tokyo.ac.jp>
2026-06-03 01:14:14 +0900

    [control] add wrench estimation and nav interface updates

--- aerial_robot_estimation/include/aerial_robot_estimation/sensor/imu.h
+++ aerial_robot_estimation/include/aerial_robot_estimation/sensor/imu.h
@@ -31,7 +31,7 @@
  *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
  *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  *  POSSIBILITY OF SUCH DAMAGE.
- *********************************************************************/
+*********************************************************************/
 
 #pragma once
 
@@ -60,10 +60,37 @@ namespace sensor_plugin
 
     inline ros::Time getStamp(){return imu_stamp_;}
 
+    inline tf::Vector3 getFilteredOmegaCog()
+    {
+      boost::lock_guard<boost::mutex> lock(omega_mutex_);
+      return filtered_omega_cog_;
+    }
+
+    inline tf::Vector3 getFilteredVelCog()
+    {
+      boost::lock_guard<boost::mutex> lock(vel_mutex_);
+      return filtered_vel_cog_;
+    }
+
+    void setFilteredOmegaCog(const tf::Vector3 filtered_omega_cog)
+    {
+      boost::lock_guard<boost::mutex> lock(omega_mutex_);
+      filtered_omega_cog_ = filtered_omega_cog;
+    }
+
+    void setFilteredVelCog(const tf::Vector3 filtered_vel_cog)
+    {
+      boost::lock_guard<boost::mutex> lock(vel_mutex_);
+      filtered_vel_cog_ = filtered_vel_cog;
+    }
+    
+    inline void treatImuAsGroundTruth(bool flag) { treat_imu_as_ground_truth_ = flag; }
+
   protected:
     ros::Publisher  acc_pub_;
     ros::Publisher  imu_pub_;
     ros::Subscriber imu_sub_;
+    ros::Publisher omega_filter_pub_; 
 
     /* rosparam */
     string imu_topic_name_;
@@ -86,7 +113,15 @@ namespace sensor_plugin
     std::array<tf::Vector3, 2> acc_non_bias_w_; /* the acceleration without bias in world frame for estimate_mode and expriment_mode */
     /* acc bias */
     tf::Vector3 acc_bias_b_; /* the acceleration bias in baselink frame, only use z axis  */
+    tf::Vector3 acc_bias_l_; /* the acceleration bias in level frame as to baselink frame: previously is acc_i */
     std::array<tf::Vector3, 2> acc_bias_w_; /* the acceleration bias in world frame for estimate_mode and expriment_mode*/
+    bool treat_imu_as_ground_truth_; /* whether use imu value as ground truth */
+    /* IIR filter*/
+    boost::mutex omega_mutex_;
+    boost::mutex vel_mutex_;
+    tf::Vector3 filtered_vel_cog_;
+    tf::Vector3 filtered_omega_cog_;
+    IirFilter lpf_omega_; // for gyro
 
     aerial_robot_msgs::States state_; /* for debug */
 
